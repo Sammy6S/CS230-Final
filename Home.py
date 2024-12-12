aa553af1c30fd5ebe5b02ed3384546b5f5738e47
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 02:24:41 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -30,5 +30,5 @@ for c in volc.Country:
 country_radio = st.sidebar.radio("Please select a Country: ", sorted(countries))
 volc_country = volc[(volc.Country.isin([country_radio]))]
 
-st.title("Selected Country")
+st.title(f"Volcanoes in :{country_radio.lower()}:{country_radio}")
 st.write(volc_country)

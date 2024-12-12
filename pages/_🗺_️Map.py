bc5388a960825f5d3d4b3b39c809f9af5628af37
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 14:02:02 -0500

    Update _🗺_️Map.py

--- pages/_🗺_️Map.py
+++ pages/_🗺_️Map.py
@@ -3,3 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pydeck as pdk
 import streamlit as st
+
+path = "volcanoes.csv"
+volc = pd.read_csv(path, delimiter=",", comment="%")
+volc.set_index("Volcano Number")

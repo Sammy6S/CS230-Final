20121600fa3a54f612e69ef5c80faf9b214fee4f
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 14:15:39 -0500

    Update _🗺_️Map.py

--- pages/_🗺_️Map.py
+++ pages/_🗺_️Map.py
@@ -13,13 +13,13 @@ volcLat = volc[["Latitude","Longitude"]]
 volcLat.rename(columns={"Latitude":"lat", "Longitude": "lon"}, inplace= True)
 st.map(volcLat)
 
-view_state = pdk.ViewState(latitude=volcLat["lat"].mean(), longitude=volcLat["lon"].mean(), zoom=0, pitch=50) 
+view_state = pdk.ViewState(latitude=volcLat["lat"].mean(), longitude=volcLat["lon"].mean(), zoom=5, pitch=25) 
 
 layer1 = pdk.Layer(type = 'ScatterplotLayer', data=volcLat, get_position='[lon, lat]', get_radius=2000, get_color=[200,200,0],   pickable=True)
 
 layer2 = pdk.Layer('ScatterplotLayer', data=volcLat, get_position='[lon, lat]', get_radius=300, get_color=[0,0,255], pickable=True)
 
-tool_tip = {"html": "<b>{Elevation (m)}</b>", "style": { "backgroundColor": "orange", "color": "white"}}
+tool_tip = {"html": "{Volcano Name}<br><b>{"Elevation (m)"}</b>", "style": { "backgroundColor": "orange", "color": "white"}}
 
 map = pdk.Deck(map_style='mapbox://styles/mapbox/streets-v12', initial_view_state=view_state, layers=[ layer1, layer2], tooltip= tool_tip)
 

f54643239b4a421d54d69df63fb7d8e1cacbb482
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 12:34:40 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,6 +13,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
+home = st.navigation([st.Page("map.py")])
+home.run()
+
 st.set_page_config(page_title="Global Volcanoes", page_icon=":volcano:")
 st.sidebar.title("\t Toggle :gear:")
 

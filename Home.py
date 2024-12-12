ae8fbcac887208022860c61e6f8405661330a3c1
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 11:24:46 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -50,6 +50,5 @@ def findMaxMin(lst, m = "max"):
 
 elevation_slider = st.slider("Slide for Elevation (+/-50m)",findMaxMin(volc["Elevation (m)"], "min") , findMaxMin(volc["Elevation (m)"], "max"), 0, 100)
 volc_elevation = volc[(volc["Elevation (m)"] >= (elevation_slider - 50)) & (volc["Elevation (m)"] <= (elevation_slider + 50))]
-temp = volc_elevation.pop("Elevation (m)")
-volc_elevation.insert(0,temp)
+volc_elevation.insert(0, "Elevation (m)", volc_elevation.pop("Elevation (m)"))
 st.write(volc_elevation.sort_values("Elevation (m)"))

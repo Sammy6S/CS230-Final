35f9587b96c881ebd007f617dcd29a759558ca90
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 00:40:15 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,14 @@
-
+"""
+Name 
+"""
 
 import pandas as pd
 import numpy as np
 import streamlit as st
 
 st.set_page_config(page_title="Global Volcanoes", page_icon=":volcano:")
+st.sidebar.write("## Upload and download :gear:")
+
 st.title("Global Volcanoes")
 path = "volcanoes.csv"
 volc = pd.read_csv(path)

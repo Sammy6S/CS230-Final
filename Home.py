2a14ef57cd69191c95ddd67f8a1a5048d771f3ca
Sammy6S <sbai@falcon.bentley.edu>
2024-12-12 01:25:48 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,8 +14,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Global Volcanoes", page_icon=":volcano:")
 st.sidebar.title("\t Toggle :gear:")
-country_radio = st.radio("Please select a Country: ", langs)
-st.sidebar.write(country_radio)
 
 st.title("Global Volcanoes")
 path = "volcanoes.csv"
@@ -23,3 +21,9 @@ volc = pd.read_csv(path, comment="-", delimiter=",")
 volc.set_index("Volcano Number", inplace=True)
 
 st.write(volc)
+
+countries=[]
+
+country_radio = st.radio("Please select a Country: ", countries)
+st.sidebar.write(country_radio)
+
